57d56cfdce69cd35260a56543039c1954ce73a5c
Moosa <computerknowledge817@gmail.com>
2023-09-28 18:19:50 +0500

    added pymysql

--- template_project/settings.py
+++ template_project/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 TEMPLATES = 'templates'
 
@@ -75,12 +79,18 @@ WSGI_APPLICATION = 'template_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# this gets overwritten
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# requires a env for mysql str
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=60, ssl_require=False)
 
 
 # Password validation
